2c278750a1b95cbbe783471d29b18aaff222aa9f
Chau <tictacpc@gmail.com>
2018-02-07 02:51:24 +0700

    Create drawer menu

--- screen/SlideMenuScreen.js
+++ screen/SlideMenuScreen.js
@@ -5,8 +5,30 @@ import {
     Platform,
     StyleSheet,
     Text,
-    View, Button, Image
+    View, Button, Image, FlatList, TouchableOpacity
 } from 'react-native';
+
+class ListItem extends Component {
+    onPress = () => {
+        this.props.onPressItem(this.props.key);
+    };
+
+    render() {
+        console.log(this.props.image);
+        return (
+            <TouchableOpacity onPress={this.onPress}>
+                
+                <View style={styles.itemView}>
+                    <Image style={{ height: 25, width: 25}}
+                    source={this.props.image} />
+                    <Text style={{ fontSize: 18, color: 'white', paddingLeft: 10, alignItems: 'center', }}>
+                        {this.props.id}
+                    </Text>
+                </View>
+            </TouchableOpacity>
+        );
+    }
+}
 
 export default class SlideMenuScreen extends Component {
     // static navigationOptions = {
@@ -19,19 +41,70 @@ export default class SlideMenuScreen extends Component {
     //     ),
     // };
 
+    renderItem = ({ item }) => (
+        <ListItem
+            id={item.key}
+            // onPressItem={this.onPressItem}
+            image={item.image}
+        />
+    );
+
     render() {
         //console.log(this.props);
+
         return (
-            <View style={styles.container} ></View>
+            <View style={styles.container} >
+                <FlatList
+                    data={dataItem}
+                    renderItem={this.renderItem}
+                />
+            </View>
         );
     }
 }
+
+const dataItem = [
+    {
+        key: 'My Profile',
+        image: require('./img/login.png')
+    },
+    {
+        key: 'Notifications',
+        image: require('./img/notification.png')
+    },
+    {
+        key: 'Settings',
+        image: require('./img/settings.png')
+    },
+    {
+        key: 'About Us',
+        image: require('./img/about.png')
+    },
+    {
+        key: 'Rate Our App',
+        image: require('./img/star.png')
+    },
+    {
+        key: 'Refer a Friend',
+        image: require('./img/refer_a_friend.png')
+    },
+    {
+        key: 'Log out',
+        image: require('./img/log_out.png')
+    },
+];
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
+        alignItems: 'flex-start',
+
         backgroundColor: '#3c3c47'
+    },
+    itemView: {
+        flex: 1,
+        flexDirection: 'row',
+        padding: 15,
     },
 });
